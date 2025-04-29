a8612dfc5a2b33db572624adcdc4fe125080248e
Libin Cheeran <libincheeran@Mac.attlocal.net>
2025-04-29 02:16:41 -0700

    chore: update database dump with latest data

--- sql/leetcode_data.sql
+++ sql/leetcode_data.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Linux (aarch64)
---
--- Host: localhost    Database: leetcode
--- ------------------------------------------------------
--- Server version	8.0.42
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `leetcode_problems`
---
+-- MySQL dump
+-- Database: leetcode
 
 DROP TABLE IF EXISTS `leetcode_problems`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `leetcode_problems` (
   `id` int NOT NULL AUTO_INCREMENT,
   `comments` varchar(255) DEFAULT NULL,
@@ -29,26 +9,16 @@ CREATE TABLE `leetcode_problems` (
   `problem_id` int NOT NULL,
   `scheduled_time` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
 -- Dumping data for table `leetcode_problems`
---
 
 LOCK TABLES `leetcode_problems` WRITE;
-/*!40000 ALTER TABLE `leetcode_problems` DISABLE KEYS */;
-INSERT INTO `leetcode_problems` VALUES (1,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-05-02 08:23:51.210840'),(2,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-05-18 08:23:51.210840'),(3,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-08-27 08:23:51.210840'),(4,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-05-08 08:23:51.210840'),(5,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-12-05 08:23:51.210840'),(6,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-06-13 08:23:51.210840'),(7,'https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days','Diameter of Binary Tree',543,'2025-05-28 08:23:51.210840');
-/*!40000 ALTER TABLE `leetcode_problems` ENABLE KEYS */;
+INSERT INTO `leetcode_problems` VALUES (29,'periodic','periodic',500,'2025-05-18 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (30,'periodic','periodic',500,'2025-05-02 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (31,'periodic','periodic',500,'2025-08-27 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (32,'periodic','periodic',500,'2025-05-08 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (33,'periodic','periodic',500,'2025-12-05 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (34,'periodic','periodic',500,'2025-05-28 09:13:41.687797');
+INSERT INTO `leetcode_problems` VALUES (35,'periodic','periodic',500,'2025-06-13 09:13:41.687797');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-04-29  8:24:33
